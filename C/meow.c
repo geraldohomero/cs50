a55b8a94369cd821cdf24cbb8877c039ee0d7fa8
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-14 20:37:23 -0300

    update

---
+++ C/meow.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// int main(void)
+// {
+//     for (int i = 0; i < 3; i++)
+//     {
+//         printf("meow\n");
+//     }
+// }
+
+// forever #include <stdbool.h>
+int main(void)
+{
+    while (true)
+    {
+        printf("meow\n");
+    }
+}
+
+// {
+//     int i = 0;
+//     while (i < 3)
+//     {
+//         printf("meow\n");
+//         i++;
+//     }
+// }
+
+// {
+//     printf("meow\n");
+//     printf("meow\n");
+//     printf("meow\n");
+// }
+
+//counter = counter + 1
+//counter += 1
+//counter ++;
